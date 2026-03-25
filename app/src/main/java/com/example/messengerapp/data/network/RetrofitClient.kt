d2149d407a5b3147422adc389e000a86236ab41a
Bloodpyre <seregabogotir@mail.ru>
2026-03-25 19:54:30 +0700

    Updating BASE_URL with Amvera Server

--- app/src/main/java/com/example/messengerapp/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/messengerapp/data/network/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     // Для эмулятора Android
-    private const val BASE_URL = "http://10.0.2.2:8000/"
+    private const val BASE_URL = "https://messangerapp-bloodpyre.amvera.io/"
     // Для реального устройства: замени на IP компьютера
     // private const val BASE_URL = "http://192.168.1.xxx:8000/"
 
